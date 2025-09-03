7f6463b7ae169b5d78af64df4f1f365abb1ef1e8
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2025-09-03 11:15:25 +0300

    add test

--- ClientServerHRTest/EmployeeControllerTests.cs
+++ ClientServerHRTest/EmployeeControllerTests.cs
@@ -1,5 +1,5 @@
 using ClientServerHR.Controllers;
-using ClientServerHR.Models;
+using ClientServerHR.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
